6f5a897205b8e5d0776c2a4d1c039e82ce4820db
Evergen <info2@dreaminc.ru>
2022-08-05 15:47:33 +0300

    Добавлены рандомные числа для суммы

--- Exemple003/Program.cs
+++ Exemple003/Program.cs
@@ -1,4 +1,6 @@
-﻿int namber_a = 3;
-int namber_b = 5;
+﻿int namber_a = new Random(). Next(1,10);
+Console.WriteLine (namber_a);
+int namber_b = new Random(). Next(1,20);
+Console.WriteLine (namber_b);
 int result = namber_a + namber_b;
 Console.WriteLine(result);
