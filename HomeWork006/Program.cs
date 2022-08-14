7d63eb31ff810498931b7ce83390d3b0126c1b1f
Evergen <info2@dreaminc.ru>
2022-08-15 00:03:53 +0300

    Определение третьей цифры также со сзначениями из 4 знаков

--- HomeWork006/Program.cs
+++ HomeWork006/Program.cs
@@ -1,11 +1,14 @@
-﻿int a = new Random().Next(10,999);
+﻿int a = new Random().Next(10,9999);
 Console.WriteLine("Начальное число: " + a);
 
-if(a / 100 != 0)
+if(a / 1000 != 0)
  {
-     Console.WriteLine("Третья цияра цисла: " + (a % 10));
+     Console.WriteLine("Третья цияра цисла: " + (a % 100)/10);
  }
-
+else if (a / 100 != 0)
+ {
+     Console.WriteLine("Третья цияра цисла: " + a % 10);
+ }
 else
 {
     Console.WriteLine("Нет третьей цифры");
