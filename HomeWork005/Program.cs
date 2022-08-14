02720e1fac5e01a1927df01bc7ea92d00798dd3e
Evergen <info2@dreaminc.ru>
2022-08-14 23:52:03 +0300

    Третья цифра

---
+++ HomeWork005/Program.cs
@@ -0,0 +1,5 @@
+﻿int Num = new Random().Next(100,999);
+Console.WriteLine(Num);
+int NewNum = ((Num / 10)% 10);
+
+Console.WriteLine(NewNum);
---
+++ HomeWork006/Program.cs
@@ -0,0 +1,12 @@
+﻿int a = new Random().Next(10,999);
+Console.WriteLine("Начальное число: " + a);
+
+if(a / 100 != 0)
+ {
+     Console.WriteLine("Третья цияра цисла: " + (a % 10));
+ }
+
+else
+{
+    Console.WriteLine("Нет третьей цифры");
+}
