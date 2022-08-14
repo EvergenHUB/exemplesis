ef141d601268c2b2f08a80ca2cf2929a4ce9d6f5
Evergen <info2@dreaminc.ru>
2022-08-14 23:53:30 +0300

    Определение выходного дня

---
+++ HomeWork007/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите число от 1 до 7");
+int day = Convert.ToInt32(Console.ReadLine());
+if ((day <= 7) & (day > 0))
+{
+    if((day == 6) || (day == 7))
+    {
+        Console.WriteLine("Выходной");
+    }
+    else
+    {
+        Console.WriteLine("Будний день");
+    }
+}
+else
+{
+    Console.WriteLine("Неверное число!");
+}
